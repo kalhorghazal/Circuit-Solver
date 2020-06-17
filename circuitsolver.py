a90b5afe9fba4d91fea4a7994ffc42673b5809fb
mobinashb <38355496+mobinashb@users.noreply.github.com>
2020-06-17 22:57:36 +0430

    Create circuitsolver.py

---
+++ circuitsolver.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+from __future__ import division
+import SchemDraw as schem
+import SchemDraw.elements as e
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import scrolledtext
+import sympy
+import numpy as np
+from IPython.display import display, Math
